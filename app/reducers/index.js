6a3feb9660a623029f04d2df6a2e344cca943856
Lucca <lucca180@gmail.com>
2020-05-28 11:57:44 -0300

    cleaning files

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,14 +1,11 @@
 // @flow
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import type { HashHistory } from 'history';
-import counter from './counter';
 import globalState from './globalState';
 
-export default function createRootReducer(history: HashHistory) {
-  return combineReducers<{}, *>({
+export default function createRootReducer(history) {
+  return combineReducers({
     router: connectRouter(history),
-    counter,
     globalState
   });
 }
